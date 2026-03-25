cf26b6805317b5cc68db0987105a3c878a3cd4dd
abhiram chennupalli <abhiramchennupalli@gmail.com>
2026-03-25 17:56:10 +0530

    code reviewed and told to edit

--- app.js
+++ app.js
@@ -2,17 +2,27 @@
 const openBtn = document.getElementById("openLogin");
 const closeBtn = document.getElementById("closeLogin");
 const modal = document.getElementById("loginModal");
+const loginAnchor = document.getElementById("loginAnchor");
 
 openBtn.addEventListener("click", () => {
     modal.classList.add("active");
+    document.body.classList.add("blur-background");
 });
 
 closeBtn.addEventListener("click", () => {
     modal.classList.remove("active");
+    document.body.classList.remove("blur-background");
+});
+
+loginAnchor.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("active");
+    document.body.classList.add("blur-background");
 });
 
 modal.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.classList.remove("active");
+        document.body.classList.remove("blur-background");
     }
 });
